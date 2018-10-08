efcf37359eb118f44dd79f55c7e3b908d24eaea0
Karina O'Neill <karinaoneill@hotmail.co.uk>
2018-10-08 13:36:29 +0200

    Update ClassicCars_DataScienceProject.py

--- ClassicCars_DataScienceProject.py
+++ ClassicCars_DataScienceProject.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import re
 %matplotlib inline
 from urllib.request import urlopen   #to extract data from html files
 from bs4 import BeautifulSoup   #bs4 = Beautiful Soup v4
